b1585189aa84ee78bd9cd6c5fee997e1dd8c7317
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-17 13:49:34 +0530

    Added home

--- app.py
+++ app.py
@@ -3,3 +3,18 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/Subtitle-API/blob/main/LICENSE
+
+from flask import Flask, redirect, request, jsonify, json
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Documentation:- <a href='https://github.com/FayasNoushad/Subtitle-API'>Subtitle-API</a>"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
