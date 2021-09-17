5ff374f19b709413f850440adba9ede3b030425a
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-17 14:06:36 +0530

    feature: subtitle search results

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def home():
     return "Documentation:- <a href='https://github.com/FayasNoushad/Subtitle-API'>Subtitle-API</a>"
 
 
+@app.route("/results/", methods=['GET'])
+def subtitle_results():
+    query = request.args.get('query')
+    dict_result = results(query)
+    if results is not None:
+        return jsonify(dict_result)
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
